7d7b126b4e84f311a992d7652262d36216df9cef
dymafr <erwan1309@hotmail.com>
2020-12-17 18:37:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-c9-l4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-c9-l4)
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { CocktailListComponent } from "./cocktail-container/cocktail-list/cocktail-list.component";
+import { CocktailDetailsComponent } from "./cocktail-container/cocktail-details/cocktail-details.component";
+import { CocktailContainerComponent } from "./cocktail-container/cocktail-container.component";
+import { SelectedDirective } from "./shared/selected/selected.directive";
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CocktailListComponent,
+    CocktailDetailsComponent,
+    CocktailContainerComponent,
+    SelectedDirective
+  ],
+  bootstrap: [AppComponent],
+  providers: []
+})
+export class AppModule {}
