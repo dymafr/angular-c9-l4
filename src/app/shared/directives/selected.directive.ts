fd8d2e65e35f98d1a8e3bac8447be367cd7423fd
dymafr <erwan1309@hotmail.com>
2021-08-16 14:02:19 +0200

    1 file modified

--- src/app/shared/directives/selected.directive.ts
+++ src/app/shared/directives/selected.directive.ts
@@ -1,25 +1,30 @@
-import { Directive, HostBinding, Input, OnChanges } from "@angular/core";
+import { Directive, HostBinding, Input, OnChanges } from '@angular/core';
 
 @Directive({
-  selector: "[appSelected]"
+  selector: '[appSelected]'
 })
 export class SelectedDirective implements OnChanges {
   @Input() private appSelected: boolean;
-  @HostBinding("style.backgroundColor") private backgroundColor: string;
-  @HostBinding("style.color") private color: string;
-  @HostBinding("style.fontWeight") private fontWeight: string;
+  @HostBinding('style.backgroundColor') private backgroundColor: string;
+  @HostBinding('style.color') private color: string;
+  @HostBinding('style.fontWeight') private fontWeight: string;
 
   ngOnChanges(): void {
     if (this.appSelected) {
-      this.backgroundColor = "var(--primary)";
-      this.color = "white";
-      this.fontWeight = "500";
+      this.backgroundColor = 'var(--primary)';
+      this.color = 'white';
+      this.fontWeight = '500';
     } else {
-      this.backgroundColor = "white";
-      this.fontWeight = "400";
-      this.color = "var(--text-regular)";
+      this.backgroundColor = 'white';
+      this.fontWeight = '400';
+      this.color = 'var(--text-regular)';
     }
   }
 
-  constructor() {}
+  constructor() {
+    this.appSelected = false;
+    this.backgroundColor = 'white';
+    this.fontWeight = '400';
+    this.color = 'var(--text-regular)';
+  }
 }
